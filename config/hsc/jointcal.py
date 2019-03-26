53da37f5ff0f7e0e13b0697cf6a7f59275761713
Yusra AlSayyad <yusra@astro.princeton.edu>
2019-03-25 19:49:25 -0500

    Use only stars in internal jointcal fitting
    and limit to isolated stars with SNR>10

--- config/hsc/jointcal.py
+++ config/hsc/jointcal.py
@@ -7,3 +7,23 @@ config.astrometryRefObjLoader.load(filterMapFile)
 config.applyColorTerms = True
 
 config.colorterms.load(os.path.join(getPackageDir("obs_subaru"), "config", "hsc", "colorterms.py"))
+
+config.sourceSelector.name = 'science'
+# Use only stars because aperture fluxes of galaxies are biased and depend on seeing
+config.sourceSelector['science'].doUnresolved = True
+# with dependable signal to noise ratio.
+config.sourceSelector['science'].doSignalToNoise = True
+# Min SNR must be > 0 because jointcal cannot handle negative fluxes,
+# otherwise selection of 10 is arbitrary
+config.sourceSelector['science'].signalToNoise.minimum = 10.
+# Base SNR on CalibFlux because that is the flux jointcal that fits and must be positive
+config.sourceSelector['science'].signalToNoise.fluxField = 'slot_CalibFlux_instFlux'
+config.sourceSelector['science'].signalToNoise.errField = 'slot_CalibFlux_instFluxErr'
+# Do not trust blended sources' aperture fluxes which also depend on seeing
+config.sourceSelector['science'].doIsolated = True
+# Do not trust either flux or centroid measurements with flags (chosen from the usual QA flags for stars)
+config.sourceSelector['science'].doFlags = True
+badFlags = ['base_PixelFlags_flag_edge', 'base_PixelFlags_flag_saturated',
+            'base_PixelFlags_flag_interpolatedCenter', 'base_SdssCentroid_flag',
+            'base_PsfFlux_flag', 'base_PixelFlags_flag_suspectCenter']
+config.sourceSelector['science'].flags.bad = badFlags
